89394b0d4eb26cf47b2bbff35141297bdc772bce
Orcus <kusum.aayushi@gmail.comcls>
2023-10-08 22:28:46 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,152 @@
-import Image from 'next/image'
+import React from "react";
 
-export default function Home() {
+const Page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <section className="w-[100vw] flex flex-col items-center bg-white text-black gap-8 sm:gap-16">
+      <section className="text-center w-full sm:w-[60%] flex flex-col gap-8">
+        <h1 className="text-4xl sm:text-5xl font-semibold">
+          <span className="text-blue-900">Payment</span> Gateways
+        </h1>
+        <div className="w-48 h-48 sm:w-96 sm:h-96 bg-blue-300 self-center"></div>
+        <p className="text-lg sm:text-2xl font-semibold text-gray-600">
+          Welcome to the Payment Gateways at Xettle! We understand that the
+          choice of payment gateways is a critical decision for any e-commerce
+          business. This page provides an overview of the payment gateway
+          options we offer to ensure secure and seamless transactions for your
+          online store.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </section>
+      <section className="w-full sm:w-[85%] flex flex-col gap-8">
+        <h1 className="text-2xl sm:text-3xl font-semibold">
+          Our Payment Gateway options
+        </h1>
+        <div className="w-full flex flex-col">
+          <p className="font-bold">Debit & Credit Cards</p>
+          <div className="flex gap-1">
+            <div className="w-16 h-8 bg-orange-300"></div>
+            <div className="w-16 h-8 bg-orange-300"></div>
+            <div className="w-16 h-8 bg-orange-300"></div>
+          </div>
+
+          <div className="self-end">
+            <p className="font-bold">Debit & Credit Cards</p>
+            <div className="flex gap-1 ">
+              <div className="w-16 h-8 bg-orange-300"></div>
+              <div className="w-16 h-8 bg-orange-300"></div>
+              <div className="w-16 h-8 bg-orange-300"></div>
+            </div>
+          </div>
+
+          <p className="font-bold">Debit & Credit Cards</p>
+          <div className="flex gap-1">
+            <div className="w-16 h-8 bg-orange-300"></div>
+            <div className="w-16 h-8 bg-orange-300"></div>
+            <div className="w-16 h-8 bg-orange-300"></div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+      </section>
+      <section className="w-full sm:w-[76%] ">
+        <div className="flex flex-col gap-6 w-full">
+          <h2 className="w-[24ch] font-bold text-2xl">
+            A System Designed to Handle End to End Payments
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="flex flex-col gap-1">
+            <h3 className="font-semibold text-lg text-gray-600">
+              Accept All Payment Modes
+            </h3>
+            <p className="text-gray-500">
+              With Domestic and International Credit & Debit cards, EMIs (
+              Credit/Debit Cards & Cardless), PayLater, Netbanking from 58
+              banks, UPI and 8 mobile wallets, Razorpay provides the most
+              extensive set of payment methods.
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h3 className="font-semibold text-lg text-gray-600">
+              Accept All Payment Modes
+            </h3>
+            <p className="text-gray-500">
+              With Domestic and International Credit & Debit cards, EMIs (
+              Credit/Debit Cards & Cardless), PayLater, Netbanking from 58
+              banks, UPI and 8 mobile wallets, Razorpay provides the most
+              extensive set of payment methods.
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h3 className="font-semibold text-lg text-gray-600">
+              Accept All Payment Modes
+            </h3>
+            <p className="text-gray-500">
+              With Domestic and International Credit & Debit cards, EMIs (
+              Credit/Debit Cards & Cardless), PayLater, Netbanking from 58
+              banks, UPI and 8 mobile wallets, Razorpay provides the most
+              extensive set of payment methods.
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h3 className="font-semibold text-lg text-gray-600">
+              Accept All Payment Modes
+            </h3>
+            <p className="text-gray-500">
+              With Domestic and International Credit & Debit cards, EMIs (
+              Credit/Debit Cards & Cardless), PayLater, Netbanking from 58
+              banks, UPI and 8 mobile wallets, Razorpay provides the most
+              extensive set of payment methods.
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <h3 className="font-semibold text-lg text-gray-600">
+              Accept All Payment Modes
+            </h3>
+            <p className="text-gray-500">
+              With Domestic and International Credit & Debit cards, EMIs (
+              Credit/Debit Cards & Cardless), PayLater, Netbanking from 58
+              banks, UPI and 8 mobile wallets, Razorpay provides the most
+              extensive set of payment methods.
+            </p>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          <button className="bg-blue-400 px-6 py-3 w-max  font-medium text-white rounded-sm">
+            Sign Up Now
+          </button>
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+      <section className="w-full sm:w-[85%] flex flex-col gap-8">
+        <h1 className="text-2xl sm:text-3xl font-bold">
+          Choosing the Right Payment Gateway
+        </h1>
+
+        <div className="w-full flex justify-center gap-8 flex-wrap">
+          <div className="w-full sm:w-[30vw] shadow-2xl p-4 flex flex-col gap-3">
+            <h1 className="text-2xl font-semibold">Right Payment Gateway</h1>
+            <p className="text-lg">
+              Selecting the right payment gateway is crucial for your e-commerce
+              success. Factors to consider when choosing a payment gateway
+              include transaction fees, security, ease of integration, and the
+              payment methods it supports.
+            </p>
+            <p className="text-lg">
+              Our team of experts at Xettle can guide you through the selection
+              process, ensuring that you choose the payment gateway that aligns
+              best with your business goals and customer preferences.
+            </p>
+          </div>
+          <div className="w-full sm:w-[30vw] shadow-2xl p-4 flex flex-col gap-3">
+            <h1 className="text-2xl font-semibold">Right Payment Gateway</h1>
+
+            <p className="text-lg">
+              Ready to enhance your e-commerce store's payment processing
+              capabilities? Contact us today to discuss your requirements and
+              let us help you integrate a secure and efficient payment gateway
+              solution into your online store.
+            </p>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Page;
